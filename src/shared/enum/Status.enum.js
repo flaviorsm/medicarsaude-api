42bb200dcb4ba5485114177feaf6da4d63c2c201
Flavio Moreira <flaviorsm@gmail.com>
2022-02-07 23:31:47 -0300

    Alteração pasta build e deletar arquivos js.

--- src/shared/enum/Status.enum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.StatusEnum = void 0;
-var StatusEnum;
-(function (StatusEnum) {
-    StatusEnum[StatusEnum["ATIVO"] = 0] = "ATIVO";
-    StatusEnum[StatusEnum["SUSPENSO"] = 1] = "SUSPENSO";
-    StatusEnum[StatusEnum["INATIVO"] = 2] = "INATIVO";
-})(StatusEnum = exports.StatusEnum || (exports.StatusEnum = {}));
